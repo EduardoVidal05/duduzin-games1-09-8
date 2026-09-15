c6497264f7c15728e175536a03a253d3be1905e8
Eduardo Vidal <eduardoovidal05@gmail.com>
2026-09-15 15:30:33 -0300

    LANY

---
+++ Assets/Scripts/CoinController.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+
+public class CoinController : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            other.gameObject.SendMessage("ChangeTextCoin");
+            Destroy(gameObject);
+        }
+    }
+}
+
+
+
+
